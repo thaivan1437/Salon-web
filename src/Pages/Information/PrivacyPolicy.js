3f0ae68b381d7427a4ce5844b2670e07258183c2
thaivan1437 <thaivan1437@gmail.com>
2020-11-25 14:23:38 +0700

    test query router

--- src/Pages/Information/PrivacyPolicy.js
+++ src/Pages/Information/PrivacyPolicy.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import NavBar from '../includes/NavBar'
 import Footer from '../includes/Footer'
 import PrivacyPolicy from "../includes/Information/PrivacyPolicy";
-
+import { useRouter } from 'next/router'
 
 
 class Home extends Component {
     render() {
+        const router = useRouter()
+        const { pid } = router.query
+        console.log('pid',pid)
         return (
             this.props && this.props.params && this.props.params.headless === "1" ?
                 <div style={{ backgroundColor: "#FFF" }}>
