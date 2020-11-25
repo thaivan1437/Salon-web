60f2746b3b400b05ea7942ae02285f9480389ce1
thaivan1437 <thaivan1437@gmail.com>
2020-11-25 15:56:23 +0700

    try load page eula

--- src/Pages/Information/Eula.js
+++ src/Pages/Information/Eula.js
@@ -3,20 +3,17 @@ import NavBar from '../includes/NavBar'
 import Footer from '../includes/Footer'
 import Eula from "../includes/Information/Eula";
 
-
-
 class Home extends Component {
     render() {
         return (
-            this.props && this.props.query && this.props.query.headless === "1" ?
-                <div style={{ backgroundColor: "#FFF" }}>
-                    <Eula modal={true} />
-                </div>
-                :
+            this.props && this.props.query && this.props.query.headless !== "1" ?
                 <div style={{backgroundColor:'rgb(247, 248, 250)'}}>
                     <NavBar bg={true} />
                     <Eula />
                     <Footer />
+                </div> :
+                <div style={{ backgroundColor: "#FFF" }}>
+                    <Eula modal={true} />
                 </div>
                 
         );
